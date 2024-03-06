7d0c397731ea9713483c8fe84457f589f357d040
CARLOSMR\Carlo <csi23-cmejero@colegioaltair.net>
2024-03-06 21:39:06 +0100

    no message

---
+++ edu.LM.Ejercicio3.2902/src/main/webapp/java.js
@@ -0,0 +1,11 @@
+let array1 = [1,5 ,2 ,"d" ];
+let array2 =[ "g", "s"];
+
+array2[0] = prompt ("Primer valor: ", "a" + "\n");
+array2[1] = prompt ("Segundo valor: ", "b");
+
+
+
+document.write(typeof(array2[0])+ array2[0]);
+document.write(typeof(array2[1])+ array2[1]);
+
